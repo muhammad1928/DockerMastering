eaee031f20a706a81004b5905ab9c91f9dcfda36
Casper Kristiansson <36133918+CasperKristiansson@users.noreply.github.com>
2024-10-28 17:35:56 +0100

    Convert to different approach

--- DockerMastering/secure-secrets/verify.sh
+++ DockerMastering/secure-secrets/verify.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-if [ "$MY_SECRET_ENV" == "super-secret-value" ]; then
+response=$(echo $MY_SECRET_ENV)
+expected_value="super-secret-value"
+
+if [[ "$response" == "$expected_value" ]]; then
   exit 0
 else
   exit 1
