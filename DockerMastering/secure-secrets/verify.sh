4708aa26524206f3be63818abee58457bb2ed792
Casper Kristiansson <36133918+CasperKristiansson@users.noreply.github.com>
2024-10-28 17:53:16 +0100

    Add verification for logs instead

--- DockerMastering/secure-secrets/verify.sh
+++ DockerMastering/secure-secrets/verify.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-response=$(echo $MY_SECRET_ENV)
-expected_value="super-secret-value"
-
-if [[ "$response" == "$expected_value" ]]; then
+if docker-compose logs | grep -q "The secret is super-secret-value"; then
+  echo "Verification successful: Secret is correctly passed to the container."
   exit 0
 else
+  echo "Verification failed: Secret not found in the container logs."
   exit 1
 fi
